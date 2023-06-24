8bed6cb51e34026147f929e8c02f8b47ddc7fb0d
f-irac-odes <108071425+f-irac-odes@users.noreply.github.com>
2023-06-24 19:41:58 +0000

    added phisics

--- Svelt3D/src/lib/Basic.svelte
+++ Svelt3D/src/lib/Basic.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import * as THREE from 'three';
     import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+    import * as CANNON from 'cannon-es';
 
    function add() {
     objects.forEach(object => {
@@ -11,6 +12,7 @@
     export let cameraPosition = { x: 0, y: 0, z: 5 };
     export let sceneBackgroundColor = 0x000000; // Black color
     export let plane = true; // Exported variable to control adding the plane
+    export let planeColor;
     export let useOrbitControls = true; // Exported variable to control using OrbitControls
   
     let renderer, scene, camera, controls;
@@ -65,18 +67,71 @@
       resize();
       animate();
       add();
+
+      const world = new CANNON.World();
+      world.gravity.set(0, -9.82, 0);
+
+      const groundShape = new CANNON.Plane();
+      const groundBody = new CANNON.Body({ mass: 0 });
+      groundBody.addShape(groundShape);
+      world.addBody(groundBody);
+
+      const boxShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
+      const boxBody = new CANNON.Body({ mass: 1 });
+      boxBody.addShape(boxShape);
+      boxBody.position.set(0, 2, 0);
+      world.addBody(boxBody);
+
+      function updatePhysics() {
+        // Step the physics simulation
+        const fixedTimeStep = 1 / 60; // 60 FPS
+        const maxSubSteps = 3; // Maximum number of sub steps per frame
+        world.step(fixedTimeStep, fixedTimeStep, maxSubSteps);
+
+        // Update the position and rotation of the Three.js mesh based on the Cannon.js body
+        boxMesh.position.copy(boxBody.position);
+        boxMesh.quaternion.copy(boxBody.quaternion);
+      }
+
+      function animatePhysics() {
+        requestAnimationFrame(animatePhysics);
+
+        updatePhysics();
+      }
+
+      animatePhysics();
     });
   
     function addPlane() {
       const geometry = new THREE.PlaneGeometry(10, 10);
-      const material = new THREE.MeshBasicMaterial({ color: 0x808080, side: THREE.DoubleSide });
+      const material = new THREE.MeshBasicMaterial({ color: planeColor, side: THREE.DoubleSide });
       const plane = new THREE.Mesh(geometry, material);
+      plane.rotateX(Math.PI / 2)
+      plane.rotateY(0)
+      plane.rotateZ(0)
       scene.add(plane);
     }
+    // Create a new physics world
+  const world = new CANNON.World();
+  world.gravity.set(0, -9.82, 0); // Set gravity
+
+// Create a fixed time step for physics simulation
+  const fixedTimeStep = 1 / 60; // 60 FPS
+  const maxSubSteps = 3; // Maximum number of sub steps per frame
+
+// Create a ground plane
+  const groundShape = new CANNON.Plane();
+  const groundBody = new CANNON.Body({ mass: 0 });
+  groundBody.addShape(groundShape);
+  world.addBody(groundBody);
+
   
     function addOrbitControls() {
       controls = new OrbitControls(camera, renderer.domElement);
-      // Add any additional OrbitControls customization here
+
+    }
+    function createWorld() {
+      
     }
   
   </script>
--- Svelt3D/src/routes/+page.svelte
+++ Svelt3D/src/routes/+page.svelte
@@ -3,21 +3,26 @@
   import Basic from "../lib/Basic.svelte";
   import * as THREE from 'three'
 
-  let cameraPosition = { x: 0, y: 0, z: 5 };
+  let  objectPos = {x : 0, y: 0, z:0}
+
+  let cameraPosition = { x: 0, y: 2, z: 5 };
   let sceneBackgroundColor = 'papayawhip';
   let plane = true;
   let useOrbitControls = true;
+  let planeColor = 'orange'
 
 
   const geometry = new THREE.TorusGeometry();
   const material = new THREE.MeshStandardMaterial({color: 0x00ff00 ,
-	metalness: 0.13});
+	metalness: 1});
   const torus = new THREE.Mesh(geometry, material);
 
   let objects = [torus]
-
+  torus.position.set(1, 2, 3)
+    
 </script>
 
-<Basic {cameraPosition} {sceneBackgroundColor} {plane}  {useOrbitControls} {objects}>
+
+<Basic {cameraPosition} {sceneBackgroundColor} {plane} {planeColor} {useOrbitControls} {objects}>
 
 </Basic>
